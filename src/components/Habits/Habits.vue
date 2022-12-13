9429623bff03a1d5a8436ec7d4b4541dcce8765c
Katie Dotson <katherine.dotson10@gmail.com>
2022-12-13 14:11:40 -0500

    css for habits detail screen

--- src/components/Habits/Habits.vue
+++ src/components/Habits/Habits.vue
@@ -1,6 +1,59 @@
 <template>
-  <h1>Habits</h1>
+  <section>
+    <h1>Habits</h1>
+    <div class="habits-container">
+      <div v-for="habit in habits" class="habit-detail">
+        <div class="habit-name">{{ habit.name }}</div>
+        <div class="habit-check" v-html="habit.checkIcon"></div>
+        <div class="habit-check" v-html="habit.checkIcon"></div>
+        <div class="habit-check" v-html="habit.checkIcon"></div>
+        <div class="habit-check" v-html="habit.checkIcon"></div>
+        <div class="habit-check" v-html="habit.checkIcon"></div>
+        <div class="habit-check" v-html="habit.checkIcon"></div>
+        <div class="habit-check" v-html="habit.checkIcon"></div>
+        <div class="habit-check" v-html="habit.checkIcon"></div>
+        <div class="habit-check" v-html="habit.checkIcon"></div>
+      </div>
+    </div>
+  </section>
 </template>
 <script lang="ts">
-export default {};
+import { Habit, useHabitsStore } from "@/stores/habits";
+export default {
+  mounted() {
+    const habitsStore = useHabitsStore();
+    this.habits = habitsStore.habits;
+    habitsStore.$subscribe((_, state) => {
+      this.habits = state.habits;
+    });
+  },
+  data() {
+    return {
+      habits: [] as Habit[],
+    };
+  },
+};
 </script>
+<style scoped>
+.habits-container {
+  scroll-behavior: smooth;
+  overflow-y: hidden;
+  overflow-x: scroll;
+}
+.habit-detail {
+  display: flex;
+}
+.habit-detail > div {
+  flex-grow: 0;
+  flex-shrink: 0;
+  padding: 20px;
+  font-size: 1rem;
+  background-color: var(--color-background-soft);
+}
+.habit-detail > div.habit-check {
+  text-align: center;
+}
+.habit-detail > div.habit-name {
+  width: 180px;
+}
+</style>
