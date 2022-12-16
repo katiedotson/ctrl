b5c790019e2381f35f4970b2445f4b8e0238e14e
Katie Dotson <katherine.dotson10@gmail.com>
2022-12-16 15:19:42 -0500

    move habits table to its own component

--- src/components/Habits/Habits.vue
+++ src/components/Habits/Habits.vue
@@ -1,34 +1,12 @@
 <script setup lang="ts">
 import Modal from "../Modal/Modal.vue"
 import EditHabitDay from "./EditHabitDay.vue"
+import HabitsTable from "./HabitsTable.vue"
 </script>
 <template>
   <section>
     <h1>Habits</h1>
-    <div id="table-scroll" class="table-scroll">
-      <table id="main-table">
-        <thead>
-          <th scope="col"></th>
-          <th scope="col" v-for="habit in habits" v-bind:key="habit.id">
-            {{ habit.name }}
-          </th>
-        </thead>
-        <tbody>
-          <tr v-for="day in days" v-bind:key="day.date">
-            <th
-              class="date"
-              v-html="getFormattedDate(day)"
-              @click="dayClicked(day)"
-            ></th>
-            <td
-              v-for="habit in habits"
-              v-html="getFormattedIcon(habit, day)"
-              v-bind:key="habit.id"
-            ></td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    <habits-table :habits="habits" :days="days" @day-clicked="dayClicked" />
     <div>
       <h2>Week of: {{ getStartDateFormatted() }}</h2>
       <div class="button-container">
@@ -82,18 +60,6 @@ export default {
     }
   },
   methods: {
-    getFormattedDate(day: AppDay): string {
-      return DateTime.fromJSDate(day.date).toFormat("EEE MMM d")
-    },
-    getFormattedIcon(habit: Habit, date: any): string {
-      if (date.habitsCompleted.some((it: any) => it == habit.id)) {
-        return habit.checkIcon
-      }
-      return ""
-    },
-    dayClicked(day: AppDay) {
-      this.dateModalDay = day
-    },
     closeDateModal() {
       this.dateModalDay = undefined
     },
@@ -104,49 +70,13 @@ export default {
       const calendarStore = useCalendarStore()
       calendarStore.changeStartDate(increment)
     },
+    dayClicked(day: AppDay) {
+      this.dateModalDay = day
+    },
   },
 }
 </script>
 <style scoped>
-.table-scroll {
-  max-height: 80vh;
-  position: relative;
-  margin: 12px auto;
-  overflow: auto;
-  background: var(--color-background-soft);
-  box-shadow: 5px 10px 8px 10px #00000021;
-}
-.table-scroll table {
-  width: 100%;
-  min-width: 600px;
-  margin: auto;
-  border-spacing: 0;
-}
-.table-wrap {
-  position: relative;
-}
-.table-scroll th,
-.table-scroll td {
-  padding: 5px;
-  border: none;
-  background: var(--color-background-soft);
-  text-align: center;
-  border: 1px solid rgba(0, 0, 0, 0.093);
-}
-.table-scroll thead th {
-  position: -webkit-sticky;
-  position: sticky;
-  top: 0;
-  z-index: 3;
-}
-th:first-child {
-  position: -webkit-sticky;
-  position: sticky;
-  left: 0;
-  z-index: 2;
-  text-align: left;
-  padding: 26px 12px;
-}
 .date {
   cursor: pointer;
 }
---
+++ src/components/Habits/HabitsTable.vue
@@ -0,0 +1,98 @@
+<template>
+  <div id="table-scroll" class="table-scroll">
+    <table id="main-table">
+      <thead>
+        <th scope="col"></th>
+        <th scope="col" v-for="habit in habits" v-bind:key="habit.id">
+          {{ habit.name }}
+        </th>
+      </thead>
+      <tbody>
+        <tr v-for="day in days" v-bind:key="day.date">
+          <th
+            class="date"
+            v-html="getFormattedDate(day)"
+            @click="dayClicked(day)"
+          ></th>
+          <td
+            v-for="habit in habits"
+            v-html="getFormattedIcon(habit, day)"
+            v-bind:key="habit.id"
+          ></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+<script lang="ts">
+import type { AppDay } from "@/stores/calendar"
+import type { Habit } from "@/stores/habits"
+import { DateTime } from "luxon"
+export default {
+  props: {
+    habits: {
+      type: Array as () => Array<Habit>,
+      required: true,
+    },
+    days: {
+      type: Array as () => Array<AppDay>,
+      required: true,
+    },
+  },
+  methods: {
+    getFormattedDate(day: AppDay): string {
+      return DateTime.fromJSDate(day.date).toFormat("EEE MMM d")
+    },
+    getFormattedIcon(habit: Habit, date: any): string {
+      if (date.habitsCompleted.some((it: any) => it == habit.id)) {
+        return habit.checkIcon
+      }
+      return ""
+    },
+    dayClicked(day: AppDay) {
+      this.$emit("dayClicked", day)
+    },
+  },
+}
+</script>
+<style scoped>
+.table-scroll {
+  max-height: 80vh;
+  position: relative;
+  margin: 12px auto;
+  overflow: auto;
+  background: var(--color-background-soft);
+  box-shadow: 5px 10px 8px 10px #00000021;
+}
+.table-scroll table {
+  width: 100%;
+  min-width: 600px;
+  margin: auto;
+  border-spacing: 0;
+}
+.table-wrap {
+  position: relative;
+}
+.table-scroll th,
+.table-scroll td {
+  padding: 5px;
+  border: none;
+  background: var(--color-background-soft);
+  text-align: center;
+  border: 1px solid rgba(0, 0, 0, 0.093);
+}
+.table-scroll thead th {
+  position: -webkit-sticky;
+  position: sticky;
+  top: 0;
+  z-index: 3;
+}
+th:first-child {
+  position: -webkit-sticky;
+  position: sticky;
+  left: 0;
+  z-index: 2;
+  text-align: left;
+  padding: 26px 12px;
+}
+</style>
